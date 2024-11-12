7af1b0060d727e33d4f87fc347b36ef750205161
Arjun Earthperson <a.earthperson@openpra.org>
2024-11-12 19:50:08 +0000

    canopy PLA flatbuffer definition
    - add basic flatbuffer & json schema for canopy PLA
    - add some comments and typing support
    - add basic test for reading from fbuf files
    - add basic file writing tests
    
    Merge-request: OPENPRA-MR-165
    Merged-by: Mihai Diaconeasa <madiacon@ncsu.edu>

---
+++ tests/grammar/canopy/io/test_io_pla.py
@@ -0,0 +1,86 @@
+import os
+import unittest
+import flatbuffers
+
+from pracciolini.grammar.canopy.io import PLAType, PLAStart, PLAStartProductsVector, PLAAddNumProducts, \
+    PLAAddNumEventsPerProduct, PLAAddType, PLAAddProducts, PLAEnd, PLA
+
+
+class TestIOCanopyPLA(unittest.TestCase):
+    def test_build_and_write_pla_to_file(self):
+        builder = flatbuffers.Builder(0)
+
+        # Products vector as defined in the set_F function
+        # Encoding the product terms into bit vectors (uint8)
+        # Values correspond to:
+        # [0b01100111, 0b10011111, 0b11011011, 0b10011011, 0b00110011]
+        products = [0b01100111, 0b10011111, 0b11011011, 0b10011011, 0b00110011]
+
+        # Start the products vector in the builder
+        PLAStartProductsVector(builder, len(products))
+        # Prepend the products in reverse order, as required by FlatBuffers
+        for value in reversed(products):
+            builder.PrependUint8(value)
+        products_vector = builder.EndVector()
+
+        # Start building the PLA table
+        PLAStart(builder)
+        # Set the type to DNF (Disjunctive Normal Form)
+        PLAAddType(builder, PLAType.DNF)
+        # Add the products vector to the PLA
+        PLAAddProducts(builder, products_vector)
+        # Set the number of products
+        PLAAddNumProducts(builder, len(products))
+        # Set the number of events per product (number of bits used per product term)
+        PLAAddNumEventsPerProduct(builder, 8)
+        # End the PLA table
+        pla = PLAEnd(builder)
+
+        # Finish the buffer with the PLA as the root object and set the file identifier
+        builder.Finish(pla, file_identifier=b'BPLA')
+
+        # Get the output buffer ready to write to a file
+        buf = builder.Output()
+
+        # Write the buffer to a file named 'pla_output.bits'
+        with open('pla_output.bits', 'wb') as f:
+            f.write(buf)
+
+        print('PLA FlatBuffer successfully built and written to pla_output.bits')
+
+    def test_read_and_validate_pla_from_file(self):
+        # Read the flatbuffer from 'pla_output.bits'
+        file_path = 'pla_output.bits'
+        self.assertTrue(os.path.exists(file_path), f"File '{file_path}' does not exist. Run the write test first.")
+
+        with open(file_path, 'rb') as f:
+            buf = f.read()
+
+        # Verify that the buffer has the correct file identifier
+        if not PLA.PLABufferHasIdentifier(buf, 0):
+            self.fail("The buffer does not have the correct file identifier 'BPLA'.")
+
+        # Get the root as PLA object
+        pla = PLA.GetRootAs(buf, 0)
+
+        # Verify the Type
+        self.assertEqual(pla.Type(), PLAType.DNF, "PLA Type does not match PLAType.DNF")
+
+        # Verify the NumProducts
+        self.assertEqual(pla.NumProducts(), 5, "Number of products does not match expected value 5.")
+
+        # Verify the NumEventsPerProduct
+        self.assertEqual(pla.NumEventsPerProduct(), 8, "Number of events per product does not match expected value 8.")
+
+        # Verify the Products vector
+        expected_products = [0b01100111, 0b10011111, 0b11011011, 0b10011011, 0b00110011]
+        self.assertEqual(pla.ProductsLength(), len(expected_products), f"Products length {pla.ProductsLength()} does not match expected {len(expected_products)}.")
+
+        for i in range(pla.ProductsLength()):
+            product = pla.Products(i)
+            self.assertEqual(product, expected_products[i], f"Product at index {i} does not match expected value.")
+
+        print('PLA FlatBuffer successfully read and validated from pla_output.bits')
+
+if __name__ == '__main__':
+    unittest.main()
