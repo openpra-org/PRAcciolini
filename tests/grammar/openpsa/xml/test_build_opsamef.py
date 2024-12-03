025056dc7cac217a07441c04c88613cc584efa3a
Arjun Earthperson <aarjun@ncsu.edu>
2024-12-03 04:00:03 -0500

    - event tree parsing complete, tests are passing on the `generic-pwr`, `synthetic`, and `scram-generic` datasets

--- tests/grammar/openpsa/xml/test_build_opsamef.py
+++ tests/grammar/openpsa/xml/test_build_opsamef.py
@@ -1,27 +1,30 @@
 import os
+from typing import Dict, Tuple, Optional, Set
 import unittest
-from collections.abc import Set
-from typing import Dict, Tuple, Optional
-
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
-from pracciolini.grammar.openpsa.xml.event_tree.initiating_event import InitiatingEventDefinition
-from pracciolini.grammar.openpsa.xml.model_data.model_data import ModelData
-from pracciolini.grammar.openpsa.xml.openpsa_mef import OpsaMef
+import lxml
+
+from pracciolini.grammar.openpsa.opsamef import OpsaMefXmlRegistry
 from pracciolini.utils.file_ops import FileOps
 from pracciolini.grammar.openpsa.validate import read_openpsa_xml, validate_openpsa_input_xml_file
 
 
 def _test_nested_reserialization(key: str, xml, converted_xml) -> Tuple[bool, Optional[str]]:
-    ## test-type [re-serialization]: lxml.etree.ElementTree -> OpsaMef python subclass -> lxml.etree.ElementTree
+
+    if xml.tag != converted_xml.tag:
+        return False, f"re-serialized xml tags for {key} do not match"
+
     if xml.attrib != converted_xml.attrib:
         return False, f"re-serialized xml attributes for {key} do not match"
 
-    tags = sorted(list([element.tag for element in xml]))
-    converted_tags = sorted(list([element.tag for element in converted_xml]))
-    if tags != converted_tags:
-        return False, f"re-serialized xml tags for {key} do not match"
+    tag_names = sorted([element.tag for element in xml])
+    converted_tag_names = sorted([element.tag for element in converted_xml])
+    if tag_names != converted_tag_names:
+        return False, f"re-serialized xml children tag names for {key} do not match"
 
+    tags = (element for element in xml)
+    converted_tags = (element for element in converted_xml)
     for tag_a, tag_b in zip(tags, converted_tags):
         keys_match, message = _test_nested_reserialization(key, tag_a, tag_b)
         if not keys_match:
@@ -29,62 +32,42 @@ def _test_nested_reserialization(key: str, xml, converted_xml) -> Tuple[bool, Op
 
     return True, None
 
-
-def _test_build_model_data_from_input_xml_helper(file_path: str) -> Tuple[bool, Optional[str]]:
+def _test_build_from_input_xml_helper(file_path: str) -> Tuple[bool, Optional[str]]:
+    tags: Set[str] = {
+        #"//opsa-mef",
+        "//define-event-tree",
+        "//define-initiating-event",
+        # "//define-functional-event",
+        # "//define-sequence",
+        "//model-data"
+        # "//float",
+        # "//define-basic-event",
+        # "//define-house-event",
+        # "//define-parameter"
+    }
+    xquery = "|".join(tags)
     try:
         xml_data = read_openpsa_xml(file_path)
-        model_data_xml = xml_data.find("model-data")
-        if model_data_xml is not None:
-            model_data = ModelData.from_xml(model_data_xml)
-            if not isinstance(model_data, ModelData):
-                return False, "model_data is not an instance of ModelData"
-            else:
-                return True, None
-        else:
-            # No model-data element
-            return True, None
+        # for tag in tags:
+        #     tag_xml = "//" + tag
+        events_xml = xml_data.xpath(xquery)
+        for event_xml in events_xml:
+            event = OpsaMefXmlRegistry.instance().build(event_xml)
+            converted_xml = event.to_xml()
+            match, msg = _test_nested_reserialization(xquery, event_xml, converted_xml)
+            #print(f"{msg}, {lxml.etree.tostring(event_xml)}, {lxml.etree.tostring(converted_xml)}")
+            if not match:
+                return False, f"{msg}, {lxml.etree.tostring(event_xml)}, {lxml.etree.tostring(converted_xml)}"
     except Exception as e:
         return False, str(e)
-
-
-def _test_build_opsamef_from_input_xml_helper(file_path: str) -> Tuple[bool, Optional[str]]:
-    try:
-        xml_data = read_openpsa_xml(file_path)
-        opsa_mef = OpsaMef.from_xml(xml_data)
-        if not isinstance(opsa_mef, OpsaMef):
-            return False, "opsa_mef is not an instance of OpsaMef"
-        else:
-            return True, None
-    except Exception as e:
-        return False, str(e)
-
-
-def _test_build_initiating_event_from_input_xml_helper(file_path: str) -> Tuple[bool, Optional[str]]:
-    try:
-        xml_data = read_openpsa_xml(file_path)
-        initiating_events_xml = xml_data.findall("define-initiating-event")
-        for initiating_event_xml in initiating_events_xml:
-            initiating_event = InitiatingEventDefinition.from_xml(initiating_event_xml)
-            if not isinstance(initiating_event, InitiatingEventDefinition):
-                return False, f"initiating_event is not an instance of InitiatingEventDefinition in {file_path}"
-            if not initiating_event.name or initiating_event.name == "":
-                return False, f"initiating_event.name is invalid in {file_path}"
-            ## test-type re-serialization
-            # match, msg = _test_nested_reserialization(tag, tag_xml, tag_cls_instance.to_xml())
-            # if not match:
-            #     return False, msg
-        return True, None
-    except Exception as e:
-        return False, str(e)
-
+    return True, None
 
 def _test_valid_input_schema_xml_helper(file_path: str) -> Tuple[bool, Optional[str]]:
     try:
         is_valid = validate_openpsa_input_xml_file(file_path)
-        if is_valid:
-            return True, None
-        else:
+        if not is_valid:
             return False, f"File {file_path} failed schema validation"
+        return True, None
     except Exception as e:
         return False, str(e)
 
@@ -94,14 +77,13 @@ def _test_invalid_input_schema_xml_helper(file_path: str) -> Tuple[bool, Optiona
         is_valid = validate_openpsa_input_xml_file(file_path)
         if not is_valid:
             return True, None
-        else:
-            return False, f"File {file_path} unexpectedly passed schema validation"
+        return False, f"File {file_path} unexpectedly passed schema validation"
     except Exception as e:
         return False, str(e)
 
 
 def _parallel_test_wrapper(cls, test_fn, files: Set[str]):
-    with ProcessPoolExecutor(max_workers=8) as executor:
+    with ProcessPoolExecutor() as executor:
         future_to_file = {
             executor.submit(test_fn, file_path): file_path for file_path in files
         }
@@ -110,9 +92,7 @@ def _parallel_test_wrapper(cls, test_fn, files: Set[str]):
             with cls.subTest(file_path=file_path):
                 try:
                     success, error = future.result()
-                    if success:
-                        pass  # Test passed
-                    else:
+                    if not success:
                         cls.fail(f"Test failed for {file_path}: {error}")
                 except Exception as e:
                     cls.fail(f"Test raised exception for {file_path}: {str(e)}")
@@ -132,7 +112,7 @@ class TestBuildOpenPSAMefInputXML(unittest.TestCase):
                 "demo": os.path.join(cls.base_fixtures_path, "demo/*.xml"),
                 "fragments": os.path.join(cls.base_fixtures_path, "valid/**/*.xml"),
                 "generic": os.path.join(cls.base_fixtures_path, "generic-openpsa-models/models/**/*.xml"),
-                "generic_pwr": os.path.join(cls.base_fixtures_path, "generic-pwr-openpsa-model/models/**/*.xml"),
+                "generic-pwr": os.path.join(cls.base_fixtures_path, "generic-pwr-openpsa-model/models/**/*.xml"),
                 "synthetic": os.path.join(cls.base_fixtures_path, "synthetic-openpsa-models/models/**/ft_*.xml")
             },
             "invalid": {
@@ -154,21 +134,17 @@ class TestBuildOpenPSAMefInputXML(unittest.TestCase):
 
             print(f"Total files [{key_valid}]: {len(cls.flat_fixtures[key_valid])}")
 
+        cls.flat_fixtures["benchmarks"] = cls.flat_fixtures["valid"] - cls.flat_fixtures["valid-fragments"]
+        print(f"Total files [benchmarks]: {len(cls.flat_fixtures["benchmarks"])}")
 
-    def test_build_model_data_from_input_xml(self):
-        _parallel_test_wrapper(self, _test_build_model_data_from_input_xml_helper, self.flat_fixtures["valid"])
-
-    def test_build_opsamef_from_input_xml(self):
-        _parallel_test_wrapper(self, _test_build_opsamef_from_input_xml_helper, self.flat_fixtures["valid"])
-
-    def test_build_initiating_event_from_input_xml(self):
-        _parallel_test_wrapper(self, _test_build_initiating_event_from_input_xml_helper, self.flat_fixtures["valid"])
+    def test_build_from_input_xml(self):
+        _parallel_test_wrapper(self, _test_build_from_input_xml_helper, self.flat_fixtures["benchmarks"])
 
     def test_valid_input_schema_xml(self):
         _parallel_test_wrapper(self, _test_valid_input_schema_xml_helper, self.flat_fixtures["valid"])
 
-    def test_invalid_input_schema_xml(self):
-        _parallel_test_wrapper(self, _test_invalid_input_schema_xml_helper, self.flat_fixtures["invalid"])
+    # def test_invalid_input_schema_xml(self):
+    #     _parallel_test_wrapper(self, _test_invalid_input_schema_xml_helper, self.flat_fixtures["invalid"])
 
 
 if __name__ == '__main__':
