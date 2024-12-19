4e999808abc735916a27e91c5726f1935d3990c9
Arjun Earthperson <a.earthperson@openpra.org>
2024-12-19 12:04:34 -0500

    - WIP looping

--- tests/grammar/canopy/model/test_construct_subgraph.py
+++ tests/grammar/canopy/model/test_construct_subgraph.py
@@ -1,23 +1,17 @@
-import timeit
 import unittest
+import timeit
 
-from keras.src.layers import InputLayer
-from tensorflow.keras.layers import Concatenate
-
-import numpy as np
-import tensorflow as tf
-
-from pracciolini.grammar.canopy.model.layers import BitpackedBernoulli, BitwiseOr, BitwiseXor, Expectation, BitwiseAnd, \
-    BitwiseNot, BitwiseXnor, BitwiseNand, BitwiseNor
-from pracciolini.grammar.canopy.model.ops.monte_carlo import tally, count_bits
-from pracciolini.grammar.canopy.model.ops.sampler import generate_bernoulli, generate_bernoulli_no_bitpack
+from pracciolini.grammar.canopy.model.layers import BitpackedBernoulli, Expectation
+from pracciolini.grammar.canopy.model.ops.monte_carlo import tally
+from pracciolini.grammar.canopy.model.ops.sampler import generate_bernoulli
 from pracciolini.grammar.canopy.model.tensor import Tensor
 from pracciolini.grammar.canopy.model.subgraph import SubGraph
 
 import tensorflow as tf
 import numpy as np
 
-from pracciolini.grammar.canopy.probability import monte_carlo
+from pracciolini.grammar.canopy.probability.monte_carlo import mse_loss
+from pracciolini.grammar.canopy.sampler import sizer
 from pracciolini.grammar.canopy.utils import compute_optimal_sample_shape_for_constraints, \
     tensor_as_formatted_bit_vectors
 
@@ -573,39 +567,48 @@ class SubGraphConstructionTests(unittest.TestCase):
 
     def test_generate_bernoulli(self):
 
+        @tf.function(jit_compile=True)
         def batched_estimate(probs_: tf.Tensor,
                              num_batches_: int,
                              sample_size_: int,
                              bitpack_dtype_: tf.DType,
                              sampler_dtype_: tf.DType,
-                             acc_dtype_: tf.DType = tf.uint32):
+                             acc_dtype_: tf.DType = tf.uint64):
             batch_size_ = probs_.shape[0]
+
             event_dim_size_ = sample_size_ * tf.dtypes.as_dtype(bitpack_dtype_).size * 8
-            event_bits_in_batch_ = tf.cast(batch_size_ * event_dim_size_, dtype=acc_dtype_)
+            event_bits_in_batch_ = tf.cast(event_dim_size_, dtype=acc_dtype_)
 
             cumulative_one_bits_ = tf.constant(0, dtype=acc_dtype_)
-            updated_expected_value_ = tf.constant(0, dtype=tf.float64)
-            losses_ = []
+            updated_expected_value_ = tf.constant(0, shape=(probs_.shape[1],), dtype=tf.float64)
+            print(updated_expected_value_.shape)
+            losses_ = tf.TensorArray(size=num_batches_, dtype=tf.float64)
 
-            for batch_idx_ in range(num_batches_):
+            for batch_idx_ in tf.range(num_batches_):
                 # Generate samples
+                # [batch_size, num_events, n_sample_packs_per_probability * bitpack_dtype * 8].
                 packed_bits_ = generate_bernoulli(
                     probs=probs_,
                     n_sample_packs_per_probability=int(sample_size_),
                     bitpack_dtype=bitpack_dtype_,
                     dtype=sampler_dtype_,
                 )
+                #print(tensor_as_formatted_bit_vectors(packed_bits_))
                 one_bits_in_batch_ = tf.reduce_sum(input_tensor=tf.cast(x=tf.raw_ops.PopulationCount(x=packed_bits_), dtype=acc_dtype_), axis=-1)
+                #print(tensor_as_formatted_bit_vectors(one_bits_in_batch_))
                 cumulative_one_bits_ = cumulative_one_bits_ + one_bits_in_batch_
                 cumulative_bits_ = (tf.cast(batch_idx_, dtype=acc_dtype_) + 1) * event_bits_in_batch_
-                updated_expected_value_ = cumulative_one_bits_ / cumulative_bits_
-                updated_loss_ = tf.keras.losses.MSE(probs_, updated_expected_value_,)
+                # each row represented the expected value per batch item
+                updated_batch_expected_value_ = cumulative_one_bits_ / cumulative_bits_
 
-                # Compute MSE losses
-                losses_.append(updated_loss_.numpy())
-                print(f"Batch [{batch_idx_ + 1}] MSE loss: {updated_loss_.numpy()}, E[f(x)]: {updated_expected_value_.numpy()}")
+                # this can be reduced by averaging over all the estimates in this batch
+                updated_expected_value_ = tf.reduce_mean(updated_batch_expected_value_, axis=0)
+                print(updated_expected_value_.shape)
+                updated_batch_loss_ = mse_loss(probs_, updated_expected_value_,)
+                losses_ = losses_.write(batch_idx_, updated_batch_loss_)
+                #print(updated_expected_value_)
 
-            return losses_, updated_expected_value_
+            return losses_.stack(), updated_expected_value_
 
         #tf.profiler.experimental.start('../../../../logs')
         #tf.compat.v1.disable_eager_execution()
@@ -614,42 +617,40 @@ class SubGraphConstructionTests(unittest.TestCase):
         #sampler tensor_memory = 1, 1024, 8388608
         sampler_dtype = tf.float32
         bitpack_dtype = tf.uint8
-        num_events = 8
+        num_events = 2 ** 3
         probs = tf.constant([
-            [1.0 / (x + 2.0) for x in range(num_events)],
+            [1.0 / (2.0) for x in range(num_events)],
         ], dtype=sampler_dtype)
-
-        batch_size, sample_size, extras = compute_optimal_sample_shape_for_constraints(num_events=num_events,
-                                                                               max_bytes=int(1.5 * 2**32),
-                                                                               dtype=sampler_dtype,
-                                                                               batch_size_range=(2, None),
-                                                                               sample_size_range=(2, None),
-                                                                               bitpack_dtype=bitpack_dtype)
-        print(f"sample_size: {sample_size}, batch_size: {batch_size}")
-        print(extras)
+        # sample_sizer = sizer.optimize_batch_and_sample_size(
+        #     num_events=num_events,
+        #     max_bytes=int(2 ** 32),  # 1.5 times 4 GiB
+        #     sampled_bits_per_event_range=(2 ** 10, None),
+        #     sampler_dtype=sampler_dtype,
+        #     bitpack_dtype=bitpack_dtype,
+        #     batch_size_range=(1, None),
+        #     sample_size_range=(1, None),
+        #     total_batches_range=(1, 1),
+        #     learning_rate=1.0,
+        #     max_iterations=1000,
+        #     tolerance=1e-8,
+        # )
+        sample_sizer = {}
+        sample_sizer['total_batches'] = 1
+        sample_sizer['batch_size'] = 913
+        sample_sizer['sample_size'] = 913
 
         # Initialize cumulative statistics
-        total_batches = 5
-        losses, est_mean = batched_estimate(probs_=probs,
-                         num_batches_=total_batches,
-                         sample_size_=sample_size,
+        losses, est_mean = batched_estimate(probs_=tf.broadcast_to(probs, [sample_sizer['batch_size'], num_events]),
+                         num_batches_=sample_sizer['total_batches'],
+                         sample_size_=sample_sizer['sample_size'],
                          bitpack_dtype_=bitpack_dtype,
                          sampler_dtype_=sampler_dtype)
 
 
-        print(f"Losses: {losses}")
-        print(f"Known Probabilities: {probs.numpy()}")
-        print(f"Estimated Means    : {est_mean.numpy()}")
-        #
-        # import matplotlib.pyplot as plt
-        # # Plot the decreasing loss trends
-        # plt.figure(figsize=(10, 6))
-        # plt.plot(range(1, num_batches + 1), cumulative_losses, marker='o')
-        # plt.title('MSE Loss vs. Number of Batches Processed')
-        # plt.xlabel('Number of Batches')
-        # plt.ylabel('MSE Loss')
-        # plt.grid(True)
-        # plt.show()
+        print(f"Batch Losses: {losses}")
+        print(f"Known Probabilities ({len(probs.numpy()[0])}): {probs.numpy()}")
+        print(f"Estimated Means    ({len(est_mean.numpy())}): {est_mean.numpy()}")
+
         return
         packed_bits = generate_bernoulli(
             probs=probs,
